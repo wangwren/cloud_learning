191adbf73c73ca6536c0c75fd662c95c8f6b6943
wangweiren <wangwren0906@gmail.com>
2021-07-28 10:42:13 +0800

    超时接口

--- cloud-provider-payment8001/src/main/java/com/wangwren/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8001/src/main/java/com/wangwren/springcloud/controller/PaymentController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 @RestController()
 @RequestMapping("payment")
@@ -80,6 +81,18 @@ public class PaymentController {
 
     @GetMapping("port")
     public String getPort() {
+
+        return serverPort;
+    }
+
+
+    @GetMapping("feign/timeout")
+    public String feignTimeOut() {
+        try {
+            TimeUnit.SECONDS.sleep(3);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
         return serverPort;
     }
